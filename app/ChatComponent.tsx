0c1935b58c452a5a80bb4123b8deee332003310d
노은수 <no-eunsu@no-eunsuui-MacBookPro.local>
2025-02-06 18:43:42 +0900

    fix

--- app/ChatComponent.tsx
+++ app/ChatComponent.tsx
@@ -1,18 +1,21 @@
-"use client"; // ✅ 클라이언트 전용 강제 선언
+"use client";
 
 import { useEffect } from "react";
-import CoxwaveChatSdk from "coxwave-chat-sdk";
 
 const ChatComponent = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const coxwaveChatSdk = new CoxwaveChatSdk({
-        clientUrl: process.env.NEXT_PUBLIC_COXWAVE_CLIENT_URL ?? "",
-        apiKey: process.env.NEXT_PUBLIC_COXWAVE_API_KEY ?? "",
-      });
+      import("coxwave-chat-sdk").then((module) => {
+        const CoxwaveChatSdk = module.default;
 
-      coxwaveChatSdk.renderChat();
-      console.log("chatSdk initialized");
+        const coxwaveChatSdk = new CoxwaveChatSdk({
+          clientUrl: process.env.NEXT_PUBLIC_COXWAVE_CLIENT_URL ?? "",
+          apiKey: process.env.NEXT_PUBLIC_COXWAVE_API_KEY ?? "",
+        });
+
+        coxwaveChatSdk.renderChat();
+        console.log("chatSdk initialized");
+      });
     }
   }, []);
 
